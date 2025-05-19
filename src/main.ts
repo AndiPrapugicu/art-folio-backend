0b0a21e9b77cb96d2021f696179bddf285697253
Andi Prapugicu <prapugicu.andi02@yahoo.com>
2025-05-19 16:49:47 +0300

    changes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -67,7 +67,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 
   console.log(`Application running on: ${await app.getUrl()}`);
   console.log(`Static files being served from: ${uploadsConfig.root}`);
